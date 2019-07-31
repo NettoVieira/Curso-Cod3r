6c76328b4a796d014b00c577a6a0d5b8af1e0c1f
Netto Vieira <joao.fvneto@hotmail.com>
2019-07-30 21:15:57 -0300

    conhecendo mais Metedos de array

---
+++ array/arrayMetodos.js
@@ -0,0 +1,28 @@
+const pilotos = ['Vettel', 'Alonso', 'Raikkonen', 'Massa']
+pilotos.pop() //massa quebrou o carro! ou seja remove o ultimo elemtno do Array
+console.log(pilotos)
+
+pilotos.push('Verstappen') //push adiciona um valor na ultima posição do Array
+console.log(pilotos)
+
+pilotos.shift()// remove o primeiro elemento do array
+console.log(pilotos)
+
+pilotos.unshift('Hamilton')
+console.log(pilotos)
+
+//splice pode adicionar e remover de dentro do Array
+
+//adicionar 
+pilotos.splice(2, 0, 'Bottas', 'Massa')
+console.log(pilotos)
+
+//Remover
+pilotos.splice(3, 1)
+console.log(pilotos)
+
+const algunspilotos1 = pilotos.splice(2) //no array a partir do indice passado
+console.log(algunspilotos1)
+
+const algunspilotos2 = pilotos.slice(1, 4)
+console.log(algunspilotos2)
