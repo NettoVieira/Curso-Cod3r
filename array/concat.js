59b6adf33b9801dd97fdf61b9d272b85f3aca2fb
NettoVieira <joao.fvneto@hotmail.com>
2019-08-04 11:35:35 -0300

    metodo concat e imperativo vs declarativo

---
+++ array/concat.js
@@ -0,0 +1,7 @@
+const filhas = ['Ualeskah', 'Cibalena']
+const filhos = ['Uoxiton', 'uesclei']
+
+const todos = filhas.concat(filhos)
+console.log(todos, filhas, filhos)
+
+console.log([].concat([1, 2], [2, 3], 5, [[6, 7]]))
